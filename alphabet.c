49b1ff14fa9984f797a653a1d0bdab8dc9e1cd48
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 18:40:41 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+    printf("Alphabet");
+    else
+    printf("Not an alphabet");
+    return 0;
+}
