1efc87db7801f20b4df1ba621b38a7d396763a2e
yaminichalapaka <103511540+yaminichalapaka@users.noreply.github.com>
2023-06-21 18:59:48 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character");
+    scanf("%c", &c);
+    if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U' || c=='a' || c=='e' || c=='i' || c=='o' ||c=='u')
+    printf("Vowel");
+    else if(!((c>='a' && c<='z') || (c>='A' && c<='Z')))
+    printf("Invalid input");
+    else
+    printf("consonant");
+    return 0;
+}
